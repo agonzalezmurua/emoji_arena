611ce2036e4ecb484dceac571f2cd9e8b15fe12e
AguGonzalez <agustingonzalezmurua@gmail.com>
2019-07-13 16:56:49 -0400

    Refactor of guildCreate flow

--- src/discord_client/chat_events.ts
+++ src/discord_client/chat_events.ts
@@ -32,19 +32,19 @@ export default function registerChatEvents() {
       channel = await createChannel() as TextChannel
       storedGuild = await new Guild({ broadcast_channel: channel.id, guild_id: guild.id }).save()
       return guild.owner.send(`Thanks for having me in, I took the liberty to set up my own channel called ${channel.name} in your server ${guild.name}`)
-    } else { // Not first time in guild
-      // Attempts to find already existing channel with previous stored ID
-      channel = guild.channels.array().find(({ id }) => id === storedGuild!.broadcast_channel) as TextChannel
+    } // Not first time in guild
+      
+    // Attempts to find already existing channel with previous stored ID
+    channel = guild.channels.array().find(({ id }) => id === storedGuild!.broadcast_channel) as TextChannel
 
-      if (channel === undefined) { // Channel was deleted or not found
-        channel = await createChannel() as TextChannel
-        storedGuild.broadcast_channel = channel.id
-        await storedGuild.save()
-        return guild.owner.send(`Thanks for having me in, I didn't found my old channel so I took the liberty to set up a new one called ${channel.name} in ${guild.name}`)
-      } else { //Nothing changed except bot joining back
-        return guild.owner.send(`Thanks for having me in your server ${guild.name}`)
-      }
+    if (channel === undefined) { // Channel was deleted or not found
+      channel = await createChannel() as TextChannel
+      storedGuild.broadcast_channel = channel.id
+      await storedGuild.save()
+      return guild.owner.send(`Thanks for having me in, I didn't found my old channel so I took the liberty to set up a new one called ${channel.name} in ${guild.name}`)
     }
+      //Nothing changed except bot joining back
+    return guild.owner.send(`Thanks for having back me in your server ${guild.name}`)
     
   })
   console.log("Chat events registered")
